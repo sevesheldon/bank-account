5a9c13cff9d159f69f71af32d0b72efc9660f8a6
Seve Sheldon <sevesheldon@gmail.com>
2017-02-15 15:49:53 -0800

    add completed HTML, CSS, README files, and a functioning JS file that has been refactored with clearly designated business and user logic.

---
+++ README.md
@@ -0,0 +1,62 @@
+# bank-account
+
+##### Epicodus Section: JavaScript OO - bank-account: "Sheldon Credit Union"
+
+#### By Seve Sheldon
+
+## Description
+
+This application is a demonstration of constructors, instances and prototypes using JavaScript to create a simple app that can manage and edit multiple bank accounts.
+
+## Setup
+
+Install bank-account by cloning this repository.
+
+## Specs
+
+* User can create an account with their name and an initial deposit amount:
+
+  * **example input**: Name:'Steve McQueen'; Initial Deposit: '$200'
+  * **example output**: Steve McQueen; Current Balance: '$200'
+
+* User can deposit and/or withdraw from their account:
+
+  * **example input**: Deposit:'50'; Withdraw:'25'
+  * **example ouput**: Current Balance: '$225'
+
+* Multiple accounts can be created. The account selected is the one displayed and edited by the deposits and withdrawals.
+
+## Technologies Used
+HTML
+
+CSS
+
+Bootstrap
+
+JavaScript
+
+jQuery
+
+### Legal
+
+Copyright (c) 2017 **Epicodus**
+
+This software is licensed under the MIT license.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,36 @@
-
 //business logic
+
+var accounts = [];
 
 function BankAccount(name, deposit) {
   this.customerName = name;
   this.balance = deposit;
 }
 
-BankAccount.prototype.deposit = function(amount) {
-  this.balance += amount;
+BankAccount.prototype.deposit = function(something) {
+  this.balance += something;
 }
 
-BankAccount.prototype.withdraw = function(amount) {
-  this.balance -= amount;
+BankAccount.prototype.withdraw = function(something) {
+  if (something > this.balance){
+    alert("You cannot overdraw your account!");
+  } else {
+    this.balance -= something;
+  }
 }
 
+var displayAccount = function(account) {
+  $("#account-display").show();
+  var index = accounts.indexOf(account);
+  $("form#edit-bank-account").attr("data-acct", index);
+  $("#account-display h2").text(account.customerName);
+  $("#show-balance").text(" $" + account.balance.toFixed(2));
+}
 
+//resets all "input" fields
+var resetFields = function() {
+  $("input").val("");
+}
 
 //user-interface
 $(document).ready(function() {
@@ -25,12 +41,37 @@ $(document).ready(function() {
     var inputtedDeposit = parseFloat($("input#new-deposit").val());
     var newBankAccount = new BankAccount(inputtedName, inputtedDeposit);
 
+    accounts.push(newBankAccount);
+    displayAccount(newBankAccount);
 
-    console.log(inputtedName);
-    console.log(inputtedDeposit);
-    console.log(typeof(inputtedDeposit));
-    console.log(newBankAccount);
+    $("ul#show-accounts").append('<li><span class="account">' + newBankAccount.customerName + '</span></li>')
 
+    $(".account").last().click(function() {
+      displayAccount(newBankAccount);
+    });
+
+    $("#accounts").show();
+    resetFields("form#new-bank-account");
+  });
+
+  $("form#edit-bank-account").submit(function(event) {
+      event.preventDefault();
+
+      var userDeposit = parseFloat($("#deposit").val());
+      var userWithdrawal = parseFloat($("#withdrawal").val());
+
+      index = $("form#edit-bank-account").attr("data-acct");
+      currentBankAccount = accounts[index];
+
+      if (userDeposit) {
+        currentBankAccount.deposit(userDeposit);
+      }
+      if (userWithdrawal) {
+        currentBankAccount.withdraw(userWithdrawal);
+      }
+
+      displayAccount(currentBankAccount);
+      resetFields("form#edit-bank-account");
   });
 
 });
